6150fbca2989767e403c9af90e05ebb32bdb1f35
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2022-01-20 22:22:51 +0400

    edit: minor changes to the index.html and style.css files

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ console.log(`this works`)
 // settingWheel = this.document.getElementById("settings-wheel")
 
 // light-dark-switch-scroll.classList.toggle("setting-wheel-after-click");
-// light-dark-switch-scroll
-// light-dark-switch-scroll-click
+// // light-dark-switch-scroll
+// // light-dark-switch-scroll-click
+
+// document.querySelector(`#light-dark-switch`).addEventListener('click', darkModeWheel);
+
+// function darkModeWheel () {
+
+//     document.querySelector(`.light-dark-switch-scroll`).classList.toggle(`.light-dark-switch-scroll`);
+//     document.querySelector(`#light-dark-switch-scroll`).classList.remove(`light-dark-switch-scroll-click`)
 
 
+//     // const switchScroll = document.getElementById(`light-dark-switch-scroll`);
+//     // console.log("the function works")
+//     // switchScroll.classList.add("light-dark-switch-scroll-click");
+//     // switchScroll.classList.remove("light-dark-switch-scroll-click");
 
-function darkModeWheel(settingWheel) {
-    const switchScroll = this.document.getElementById(`light-dark-switch-scroll`) ;
-    // const
-    // console.log(`the function works`);
 }
