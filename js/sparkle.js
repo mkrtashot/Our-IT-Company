819716c774b5e257fb8ee4925b4c5516ce2f894c
GEVORG KEROBYAN <kerobyangevorg@gmail.com>
2022-01-30 23:22:16 +0400

    feat: home page polish

---
+++ js/sparkle.js
@@ -0,0 +1,25 @@
+// homePage sparkling animation //
+
+const app = document.getElementById("sparkle");
+
+const myRand = () => {
+	let r = 50;
+	while (40 < r && r < 60) {
+		r = Math.random() * 100;
+	}
+	return r;
+};
+
+for (let i = 0; i < 50; i++) {
+	const delay = Math.random() + "s";
+	const el = document.createElement("img");
+	el.src = "../pictures/sparkle-effect-white-square.jpg";
+	el.className = "glitter-star";
+	el.style.top = myRand() + "%";
+	el.style.left = myRand() + "%";
+	el.style.animationDelay = delay;
+	el.style.msAnimationDelay = delay;
+	el.style.webkitAnimationDelay = delay;
+	el.style.monAnimationDelay = delay;
+	app.appendChild(el);
+}
